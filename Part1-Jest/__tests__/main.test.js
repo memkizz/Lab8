165ffca8019b7a5393f20246658fc51091ce372f
memkizz <kwstubbings@protonmail.com>
2021-02-26 21:34:23 -0800

    done

---
+++ Part1-Jest/__tests__/main.test.js
@@ -0,0 +1,15 @@
+const re = require('../assets/scripts/main.js');  
+describe('Test all possible branchjes of the volumeIcon', () => {
+    test('high volume', () => {
+        expect(formatVolumeIconPath(100)).toEqual(`./assets/media/icons/volume-level-3.svg`);
+    });
+    test('medium volume', () => {
+        expect(formatVolumeIconPath(40)).toEqual(`./assets/media/icons/volume-level-2.svg`);
+    });
+    test('low volume', () => {
+        expect(formatVolumeIconPath(10)).toEqual(`./assets/media/icons/volume-level-1.svg`);
+    });
+    test('no volume', () => {
+        expect(formatVolumeIconPath(0)).toEqual(`./assets/media/icons/volume-level-0.svg`);
+    });
+});
